abf5bf062de934a07f746133d5f2f4f733d317cd
Jimmyd72 <81271541+JimmyD72@users.noreply.github.com>
2021-06-11 03:41:15 +0000

    I added the second part, cleaned up code from first part, the app ran fine and then it started throwing an error at line 64, about a semicolon that is supposed to be there, but the program says no. I haven't messed with anything below the warning, so I dont have a clue what to do here

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,51 +1,57 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
+let candidateName = " ";
 
-// TODO 1.1a: Define candidateName // 
-let candidateName = (" ");
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = (input.question(question + ""));
-let questions = ("Who was the first American woman in space?","True or false: 5 kilometer = 5000 meters? ","(5 + 3)/2 * 10 = ?" , "Given the array [8,'Orbit','Trajectory', 45],'what entry is at index 2?","What is the minimum crew size for the ISS? ");  
-let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
-let candidateAnswers = [];
+let question = 'Who was the first American woman in space? ';
+let correctAnswer = 'Sally Ride';
+let candidateAnswer = ' ';
+let questions = ["Who was the first American woman in space?", "True or False: 5 Kilometers==5000 meters?", "(5+3)/2*10=", "Given the array:[ 8, 'orbit' 'trajectory' 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"]
+let correctAnswers = ['Sally Ride', 'True', '40','Trajectory', '3'];
+let candidateAnswers = []
 
-console.log()
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-let candidateName = (input.question("What is your Name? "))
+candidateName = input.question("What is your name, candidate? ");
 }
-
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question("Who was the first American woman in space? .");
+
+for (let i=0; i< question.length; i++){
+candidateAnswer = input.question(question);  
+candidateAnswers.push(candidateAnswer);
+}
+if (candidateAnswer===correctAnswer) {
+console.log("That is correct!");
+} else {
+console.log("You have chosen poorly");
 }
 
-function gradeQuiz(candidateAnswers) {
+
+
+function gradeQuiz() {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade 
-    if (correctAnswer === candidateAnswer){;
-    console.log("Correct")
-    } else { 
-    console.log("Incorrect")
-    }
-     return grade;
+{
+  let grade;
+  
+
+  return grade;
 }
 
 function runProgram() {
+  askForName();
+  // TODO 1.1c: Ask for candidate's name //
   
-
-
+  console.log("Hello, ",+candidateName,+"glad you could join us")
   askQuestion();
-
+  gradeQuiz(this.candidateAnswers);
+}
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
+
 module.exports = {
   candidateName: candidateName,
   question: question,
@@ -57,7 +63,3 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
-
-let correctAnswers;
-let candidateAnswers;
-
