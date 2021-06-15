d7eef402c0b974d8a9954ed4a2a998f6054f8bbd
Jimmyd72 <81271541+JimmyD72@users.noreply.github.com>
2021-06-15 16:47:27 +0000

    I swear this has got to be right

--- candidate-testing.js
+++ candidate-testing.js
@@ -20,7 +20,7 @@ function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   let candidateName = input.question("What is your name, candidate? ");
   console.log("Candidate name: " + candidateName);
-  console.log("\n");
+  
 }
 
 function askQuestion() {
